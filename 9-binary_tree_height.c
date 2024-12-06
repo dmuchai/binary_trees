c698d41f64805e6a0705f99dff93fef29fccc83f
Your Name <dmmuchai@gmail.com>
2024-12-06 02:23:09 -0800

    Task 9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,6 +9,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
