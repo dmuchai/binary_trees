47c36b0fd9be453dcd4c16cf07009cb660f1789c
Your Name <dmmuchai@gmail.com>
2024-12-06 05:30:35 -0800

    Task 17. Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,14 +8,14 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-    if (node->parent->left == node)
-        return (node->parent->right);
+	if (node->parent->left == node)
+		return (node->parent->right);
 
-    if (node->parent->right == node)
-        return (node->parent->left);
+	if (node->parent->right == node)
+		return (node->parent->left);
 
-    return (NULL); /* Should never reach here, included for completeness */
+	return (NULL);
 }
